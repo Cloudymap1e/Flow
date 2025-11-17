bd8df1eba053300ce9bbf2294f8ecb101f593e4b
BlackMaple <j.arms1107@gmail.com>
2025-11-18 03:51:45 +0800

    Initial Commit

---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by r c on 2025/11/18.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
